3e9928b9adbd712f71bf0181708ce1c1adb0637a
rohit joshi <developerjoshi577@gmail.com>
2023-06-30 12:05:08 +0530

    links changed to internal pages testing

--- popup/pop.js
+++ popup/pop.js
@@ -4,7 +4,7 @@ function Pop({hide}) {
   return (
     <>
     <div>
-    <iframe src="https://www.turnintern.com" width="90%" height="500px" style={{marginLeft :"5%"}}></iframe>
+    <iframe src="https://www.turnintern.com/cengage-presentation/page/1" width="90%" height="500px" style={{marginLeft :"5%"}}></iframe>
     </div>
 
     <button onClick={()=>{hide(false)}} style={{width :"50%" , marginLeft: "25%"
--- src/app/tab/page.js
+++ src/app/tab/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 function NewTab() {
   return (
     <div>
-    <iframe src="https://www.turnintern.com" width="90%" height="500px" style={{marginLeft :"5%"}}></iframe>
+    <iframe src="https://www.turnintern.com/cengage-presentation/page/1" width="90%" height="500px" style={{marginLeft :"5%"}}></iframe>
 
     <Link href="/">
     <button style={{width:"50%" , marginLeft:"25%"}}> go back</button>
